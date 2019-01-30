9a0a5fec7976a379015a8eb8050ee326ef792fd6
masahikomori <masahikomori@outlook.jp>
2019-01-31 03:15:51 +0900

    For now

--- js/default.js
+++ js/default.js
@@ -1,17 +1,46 @@
 $(function () {
-  var apiUrl = "https://api.github.com/users/masahikomori";
-  var name = "masahikomori.github.io";
+  // Account
+  var account = "masahikomori";
+  // GitHub API
+  var apiUrl = "https://api.github.com/users/";
+  // Repository
+  var githubUrl = "https://github.com/";
+  var githubRepoParam = "tab=repositories";
+  var sort = "sort=updated";
+  // Gists
+  var gistUrl = "https://gist.github.com/";
+  // Form
+  var formUrl = "https://" + account + ".wufoo.com/forms/contact/";
+  // Etc
   var dt = new Date();
 
-  var content = new Vue({
-    el: '#content',
+  // GitHub user info
+  $.get(apiUrl + account).then(function (lists) {
+    about.lists = lists;
+  });
+
+  // About
+  var about = new Vue({
+    el:"#about",
+    data:{
+      head:"About",
+      about:"Software engineer and Otaku. I mostly use .Net Framework.",
+      lists:[]
+    }
+  });
+
+  // Social
+  var social = new Vue({
+    el: '#social',
     data: {
+      head:"SNS",
+      description:"SNS",
       items: []
     },
     beforeCreate: function () {
-      axios.get('./content/content.json')
+      axios.get('./content/social.json')
           .then(function (response) {
-            content.items = response.data;
+            social.items = response.data;
           })
           .catch(function (error) {
               console.log(error);
@@ -19,30 +48,72 @@ $(function () {
     }
   })
 
-  var user = new Vue({
-    el:"#user",
+  // Activity
+  var activity = new Vue({
+    el: '#activity',
+    data: {
+      head:"Activity",
+      description:"最近の活動",
+      items: []
+    },
+    beforeCreate: function () {
+      axios.get('./content/activity.json')
+          .then(function (response) {
+            activity.items = response.data;
+          })
+          .catch(function (error) {
+              console.log(error);
+          });
+    }
+  })
+
+  // GitHub repository
+  var repos = new Vue({
+    el:"#repo",
     data:{
-      lists:[]
+      head:"GitHub recent update",
+      description:"リポジトリの更新情報",
+      lists:[],
+      more:githubUrl  + account + "?" + githubRepoParam + "&" + sort
     }
   });
 
-  // GitHub user info
-  $.get(apiUrl).then(function (lists) {
-    user.lists = lists;
+  $.get(apiUrl + account + "/repos?" + sort).then(function (lists) {
+    repos.lists = lists;
   });
 
-  // Title
-  var title = new Vue({
-    el:"#title",
-      data:{text:name}
+  // GitHub Gists
+  var gists = new Vue({
+    el:"#gist",
+    data:{
+      head:"Gist recent update",
+      description:"Gistの更新情報",
+      lists:[],
+      more:apiUrl + account + "/gists"
+    }
   });
-  
+
+  $.get(apiUrl + account + "/gists").then(function (lists) {
+    gist.lists = lists;
+  });
+
+  // Contact
+  var contact = new Vue({
+    el:"#contact",
+    data:{
+      head:"Contact",
+      description:"お問い合わせ",
+      about:"ご要望やメッセージなど、こちらのフォームにてご連絡ください。Emailによるご連絡をご希望される場合は、masahikomori[at]outlook.jp をご利用ください。",
+      url:formUrl,
+    }
+  });
+      
   // Navigation bar
   var navbarElem = Vue.extend({
     template: "<nav class='navbar navbar-inverse navbar-fixed-top'>" +
               "<div class='navbar-header'><a class='navbar-brand'>" +
               "<div id='nav-title'>" +
-              name +
+              account +
               "</div></a></div></nav>"
   });
 
@@ -50,10 +121,10 @@ $(function () {
   var navigation = new Vue({
     el: "#navigation",
   });
-
+  
   // Footer
   var footerElem = Vue.extend({
-    template: "<p class='text-center'>&copy;&nbsp;"+ dt.getFullYear() + "&nbsp;" + name + "</p>"
+    template: "<p class='text-center'>&copy;&nbsp;"+ dt.getFullYear() + "&nbsp;" + account + "</p>"
   });
 
   Vue.component("elem", footerElem)
